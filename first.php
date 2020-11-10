cfb9daa7009a6826f3808d971aac18d9141db2a4
diedrebrown <diedre@diedrebrown.com>
2020-11-10 13:16:58 -0500

    Update first.php

--- first.php
+++ first.php
@@ -2,7 +2,7 @@
 <body>
 <?php
 echo "Hello world! These are my files for INFO 638";
-echo "I am making a new change. this is another change";
+echo "I am making a new change. this is another changes ";
 ?>
 </body>
 </html>
